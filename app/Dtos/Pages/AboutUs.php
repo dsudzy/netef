dc0c34e9d54e3793ad18c2757c17d4e89e522fc0
Dan Sudenfield <dansudenfield@Dans-MacBook-Pro.local>
2021-01-12 21:30:52 -0500

    finish about us

--- app/Dtos/Pages/AboutUs.php
+++ app/Dtos/Pages/AboutUs.php
@@ -4,25 +4,38 @@ namespace App\DTOs\Pages;
 
 use App\Dtos\Header;
 use App\Dtos\MetaData;
+use App\Dtos\DonateButton;
 
-class Grants {
+class AboutUs {
     public $header;
     public $meta_data;
-    public $text_block;
+    public $donate_button;
     public $board_application_link;
     public $board_of_directors_text;
+    public $button_1_text;
+    public $button_1_link;
+    public $button_2_text;
+    public $button_2_link;
 
     public function __construct(
         MetaData $meta_data,
         Header $header,
         DonateButton $donate_button,
         ?string $board_application_link,
-        ?string $board_of_directors_text
+        ?string $board_of_directors_text,
+        ?string $button_1_text,
+        ?string $button_1_link,
+        ?string $button_2_text,
+        ?string $button_2_link
     ) {
         $this->header = $header;
         $this->meta_data = $meta_data;
-        $this->text_block = $text_block;
+        $this->donate_button = $donate_button;
         $this->board_application_link = $board_application_link;
         $this->board_of_directors_text = $board_of_directors_text;
+        $this->button_1_text = $button_1_text;
+        $this->button_1_link = $button_1_link;
+        $this->button_2_text = $button_2_text;
+        $this->button_2_link = $button_2_link;
     }
 }
--- app/Http/Controllers/AboutUsController.php
+++ app/Http/Controllers/AboutUsController.php
@@ -6,6 +6,7 @@ use App\Models\LaraPage;
 use Illuminate\Support\Str;
 use App\Dtos\{
     Pages\AboutUs,
+    DonateButton
 };
 
 /**
@@ -34,10 +35,10 @@ class AboutUsController extends Controller {
             abort(404);
         }
 
-        // $page_content = $this->buildContent($page);
+        $page_content = $this->buildContent($page);
 
         $data = [
-            // 'content' => $page_content,
+            'content' => $page_content,
             'body_classes' => self::$body_class,
         ];
 
@@ -51,8 +52,23 @@ class AboutUsController extends Controller {
         $meta_data_array = $this->getMetaData($page);
         $meta_data_dto = $this->buildMetaDataDto($meta_data_array);
         $header = $this->buildHeaderDto($meta_data_array);
+        $donate_button = $this->buildDonateButtonDto($meta_data_array);
 
-        return new AboutUs($meta_data_dto, $header, );
+        return new AboutUs(
+            $meta_data_dto,
+            $header,
+            $donate_button,
+            $meta_data_array["au_board_application_link"],
+            $meta_data_array["au_board_of_directors_text"],
+            $meta_data_array["au_button_1_text"],
+            $meta_data_array["au_button_1_link"],
+            $meta_data_array["au_button_2_text"],
+            $meta_data_array["au_button_2_link"]
+        );
+    }
+
+    private function buildDonateButtonDto($meta_data) {
+        return new DonateButton($meta_data["au_donate_button_text"], $meta_data["au_donate_button_link"]);
     }
 
 }
