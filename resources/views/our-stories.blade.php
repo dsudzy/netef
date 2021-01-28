3b5505aad6a745516190049cbcab0af282fae567
Dan Sudenfield <dansudenfield@Dans-MacBook-Pro.local>
2021-01-28 11:13:07 -0500

    stories layout. donate button with shadow

--- resources/views/our-stories.blade.php
+++ resources/views/our-stories.blade.php
@@ -25,7 +25,7 @@
         <div class="grid-x">
             <div class="cell">
                 <div>
-                    <h2>story 1</h2>
+                    <h2>our stories</h2>
                     <p>Pudam quamus ut aliquos atestempos quis et arunt hiliqui quatetur sum vel mos earcitis sum eum rehenda eptissi re officium remodit, nulpa natures deniet lignatu riosti odi verovit, quae lab ilis maximod igendit voluptate nossit autenim olenihicab idionse quissition repudis.</p>
                 </div>
             </div>
@@ -38,7 +38,7 @@
                 </div>
                 <div class="cell large-6 text-wrapper">
                     <div>
-                        <h2>story 2</h2>
+                        <h2>story 1</h2>
                         <p>Pudam quamus ut aliquos atestempos quis et arunt hiliqui quatetur sum vel mos earcitis sum eum rehenda eptissi re officium remodit, nulpa natures deniet lignatu riosti odi verovit, quae lab ilis maximod igendit voluptate nossit autenim olenihicab idionse quissition repudis.</p>
                     </div>
                 </div>
@@ -47,37 +47,37 @@
 
         <div class="stories-wrapper">
             <div class="grid-x">
+                <div class="cell large-6 text-wrapper">
+                    <div>
+                        <h2>story 2</h2>
+                        <p>Pudam quamus ut aliquos atestempos quis et arunt hiliqui quatetur sum vel mos earcitis sum eum rehenda eptissi re officium remodit, nulpa natures deniet lignatu riosti odi verovit, quae lab ilis maximod igendit voluptate nossit autenim olenihicab idionse quissition repudis.</p>
+                    </div>
+                </div>
+                <div class="cell large-6 image-wrapper">
+                    <img src="https://via.placeholder.com/400x400" alt="">
+                </div>
+            </div>
+        </div>
+
+        <div class="stories-wrapper">
+            <div class="grid-x">
+                <div class="cell large-6 image-wrapper">
+                    <img src="https://via.placeholder.com/400x400" alt="">
+                </div>
                 <div class="cell large-6 text-wrapper">
                     <div>
                         <h2>story 3</h2>
                         <p>Pudam quamus ut aliquos atestempos quis et arunt hiliqui quatetur sum vel mos earcitis sum eum rehenda eptissi re officium remodit, nulpa natures deniet lignatu riosti odi verovit, quae lab ilis maximod igendit voluptate nossit autenim olenihicab idionse quissition repudis.</p>
                     </div>
                 </div>
-                <div class="cell large-6 image-wrapper">
-                    <img src="https://via.placeholder.com/400x400" alt="">
-                </div>
             </div>
         </div>
 
         <div class="stories-wrapper">
             <div class="grid-x">
-                <div class="cell large-6 image-wrapper">
-                    <img src="https://via.placeholder.com/400x400" alt="">
-                </div>
                 <div class="cell large-6 text-wrapper">
                     <div>
                         <h2>story 4</h2>
-                        <p>Pudam quamus ut aliquos atestempos quis et arunt hiliqui quatetur sum vel mos earcitis sum eum rehenda eptissi re officium remodit, nulpa natures deniet lignatu riosti odi verovit, quae lab ilis maximod igendit voluptate nossit autenim olenihicab idionse quissition repudis.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="stories-wrapper">
-            <div class="grid-x">
-                <div class="cell large-6 text-wrapper">
-                    <div>
-                        <h2>story 5</h2>
                         <p>Pudam quamus ut aliquos atestempos quis et arunt hiliqui quatetur sum vel mos earcitis sum eum rehenda eptissi re officium remodit, nulpa natures deniet lignatu riosti odi verovit, quae lab ilis maximod igendit voluptate nossit autenim olenihicab idionse quissition repudis.</p>
                     </div>
                 </div>
