d84963c269551b33c4459653fa35e5f95ed2ed49
Dan Sudenfield <dansudenfield@Dans-MacBook-Pro.local>
2021-01-30 10:06:43 -0500

    styled menu

--- resources/views/layouts/primary-nav.blade.php
+++ resources/views/layouts/primary-nav.blade.php
@@ -8,35 +8,34 @@
     </div>
     <div class="top-bar" id="example-menu">
         <div class="top-bar-left">
-            <ul class="vertical medium-horizontal menu">
-                <li class="nav-home"><a href="/">home</a></li>
-                <li>
-                    <a href="/who-we-support">who we support</a>
-                    <ul class="vertical menu menu-dropdown">
+            <ul class="menu">
+                <li class="dropdown">
+                    <a href="/who-we-support" class="top-nav-item">who we support</a>
+                    <ul class="vertical menu dropdown-content">
                         <li><a href="/njtl">NJTL</a></li>
                         <li><a href="/player-support">player support</a></li>
                         <li><a href="wheelchair-and-adaptive">wheelchair &amp; <br>adaptive</a></li>
                     </ul>
                 </li>
-                <li><a href="/our-stories">our stories</a></li>
-                <li>
-                    <a href="/about-us">about us</a>
-                    <ul class="vertical menu menu-dropdown">
+                <li class="single-nav-item"><a href="/our-stories">our stories</a></li>
+                <li class="dropdown">
+                    <a href="/about-us" class="top-nav-item">about us</a>
+                    <ul class="vertical menu dropdown-content">
                         <li><a href="/mission-and-vision">mission &amp; vision</a></li>
                         <li><a href="/board-of-directors">board of directors</a></li>
                         <li><a href="/voluneer-with-us">volunteer with us</a></li>
                     </ul>
                 </li>
-                <li>
-                    <a href="/support-us">support us</a>
-                    <ul class="vertical menu menu-dropdown">
+                <li class="dropdown">
+                    <a href="/support-us" class="top-nav-item">support us</a>
+                    <ul class="vertical menu dropdown-content">
                         <li><a href="/events">events</a></li>
                         <li><a href="/donate">donate</a></li>
                     </ul>
                 </li>
-                <li>
-                    <a href="/grants">grants</a>
-                    <ul class="vertical menu">
+                <li class="dropdown">
+                    <a href="/grants" class="top-nav-item">grants</a>
+                    <ul class="vertical menu dropdown-content">
                         <li><a href="/player-grants">player grants</a></li>
                         <li><a href="/njtl-grants">NJTL grants</a></li>
                     </ul>
@@ -45,7 +44,7 @@
         </div>
         <div class="top-bar-right">
             <ul class="menu">
-                <li>@include("partials.donate")</li>
+                <li></li>
                 <li><a href="/" class="header-logo"><img src="/img/NETEF_Informal.png" alt="Logo"></a></li>
             </ul>
         </div>
