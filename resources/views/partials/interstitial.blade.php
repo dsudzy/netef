d047d84b827c3c56978a96a8805d125b5b85424f
Dan Sudenfield <dansudenfield@Dans-MacBook-Pro.local>
2021-01-31 21:40:45 -0500

    flipped classes

--- resources/views/partials/interstitial.blade.php
+++ resources/views/partials/interstitial.blade.php
@@ -8,7 +8,7 @@
 <a href="{{ $linked_page }}">
     <div class="interstitial-item">
         <h3>{{ $title }}</h3>
-        <img class="blue-image" src="{{ $color_image }}" alt="{{ $title }} image">
-        <img class="color-image" src="{{ $grey_image }}" alt="{{ $title }} image">
+        <img class="color-image" src="{{ $color_image }}" alt="{{ $title }} image">
+        <img class="blue-image" src="{{ $grey_image }}" alt="{{ $title }} image">
     </div>
 </a>
