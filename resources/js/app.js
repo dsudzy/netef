4804f9d59def5e8a6120723e7b1a2a294cbfce6e
Dan Sudenfield <dansudenfield@Dans-MBP.hsd1.ma.comcast.net>
2021-02-03 20:35:12 -0500

    menu work

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ require('./bootstrap');
 var Draggabilly = require('draggabilly');
 
 
-$('.menu-icon').on('click', function(){
+$('.menu-icon').on('click', function() {
     $('.primary-nav').toggleClass("active");
     $('.nav-home').toggle();
 });
@@ -12,6 +12,15 @@ $(window).on('resize', function() {
         $('.primary-nav').removeClass("active");
         $('.nav-home').show();
     }
+    // if ($(window).outerWidth() < 963) {
+    //     $('.title-bar').css({ "display": "flex" });
+    //     $('.top-bar').css({ "display": "none" });
+    // } else {
+    //     $('.title-bar').css({ "display": "none" });
+    //     $('.top-bar').css({ "display": "flex" });
+    // }
+
+
 });
 
-var draggie = new Draggabilly( '.draggable', {});
+var draggie = new Draggabilly('.draggable', {});
