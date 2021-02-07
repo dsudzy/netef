90cc6df0cee6eed4442971e93a4c79fc2d16b1cd
Dan Sudenfield <dansudenfield@Dans-MBP.hsd1.ma.comcast.net>
2021-02-07 17:15:48 -0500

    menu is much better plave

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ require('./bootstrap');
 var Draggabilly = require('draggabilly');
 
 
-$('.menu-icon').on('click', function(){
+$('.menu-icon').on('click', function() {
     $('.primary-nav').toggleClass("active");
     $('.nav-home').toggle();
 });
@@ -12,6 +12,21 @@ $(window).on('resize', function() {
         $('.primary-nav').removeClass("active");
         $('.nav-home').show();
     }
+    if ($(window).outerWidth() < 768) {
+        $('.menu-bar').hide();
+    } else {
+        $('.menu-bar').show();
+        $('.top-bar').hide();
+    }
+});
+$(document).ready(function() {
+    if ($(window).outerWidth() < 768) {
+        $('.menu-bar').hide();
+    } else {
+        $('.menu-bar').show();
+        $('.top-bar').hide();
+    }
 });
 
-var draggie = new Draggabilly( '.draggable', {});
+
+var draggie = new Draggabilly('.draggable', {});
