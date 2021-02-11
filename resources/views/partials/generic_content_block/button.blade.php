c13c75f5f540ad229ff51ea0ceb8463174e28ef8
Dan Sudenfield <dansudenfield@Dans-MBP.hsd1.ma.comcast.net>
2021-02-10 19:15:01 -0500

    button link

--- resources/views/partials/generic_content_block/button.blade.php
+++ resources/views/partials/generic_content_block/button.blade.php
@@ -1,5 +1,5 @@
 <div class="grid-x generic-item-wrapper">
     <div class="cell">
-        <button class="hollow button" href="{{ $button_link }}">{{ $button_text }}</button>
+        <a href="{{ $button_link }}"><button class="hollow button">{{ $button_text }}</button></a>
     </div>
 </div>
