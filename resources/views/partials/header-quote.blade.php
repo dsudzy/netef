c1d28854960b81a2995d53ea416237e0f6dbb9b9
Dan Sudenfield <dansudenfield@Dans-MacBook-Pro.local>
2021-02-12 09:21:34 -0500

    remove icon from header quote

--- resources/views/partials/header-quote.blade.php
+++ resources/views/partials/header-quote.blade.php
@@ -1,9 +1,10 @@
 <div class="header-quote-wrapper">
     <div class="grid-x quote-content">
-        <div class="cell large-3">
+        {{-- <div class="cell large-3">
             <img src="{{ $icon }}" alt="quote icon">
         </div>
-        <div class="cell large-9">
+        <div class="cell large-9"> --}}
+        <div class="cell">
             <h4>'{!! $quote !!}'</h4>
         </div>
     </div>
