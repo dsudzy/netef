bbf7a4340fa663e6b9fd8aedab3f078bef489d16
Dan Sudenfield <dansudenfield@Dans-MacBook-Pro.local>
2021-02-13 22:28:14 -0500

    move to master content wrapper, finish page

--- resources/views/mission-and-vision.blade.php
+++ resources/views/mission-and-vision.blade.php
@@ -43,16 +43,17 @@
                     </div>
                 </a>
             </div>
+            <div class="grid-x support-wrapper">
+                <div class="cell large-4">
+                    <img src="{{ $image->getImageUrl($meta_data['support_of_our_donors_icon'] ?? 0) }}" alt="">
+                </div>
+                <div class="cell large-8 support-text">
+                    <h3>With the support of our donors</h3>
+                    <p>{!! $meta_data['support_of_our_donors_text'] !!}</p>
+                </div>
+            </div>
         <div>
-        <div class="grid-x support-wrapper">
-            <div class="cell large-4">
-                <img src="{{ $image->getImageUrl($meta_data['support_of_our_donors_icon'] ?? 0) }}" alt="">
-            </div>
-            <div class="cell large-8 support-text">
-                <h3>With the support of our donors</h3>
-                <p>{!! $meta_data['support_of_our_donors_text'] !!}</p>
-            </div>
-        </div>
+
         
         <div class="reveal mv-open-modal" id="education-modal" data-reveal>
             <div class="grid-x">
@@ -98,6 +99,9 @@
                 <span aria-hidden="true">&times;</span>
             </button>
         </div>
+        @include('partials.master-content-block-wrapper', [
+            'html_content' => $content->html_content
+        ])
     </div>
 </section>
 @endsection
