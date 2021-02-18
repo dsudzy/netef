d5a2ed46c8f7679db8f8dd400c331b8d5fa32b4b
Dan Sudenfield <dansudenfield@Dans-MBP.hsd1.ma.comcast.net>
2021-02-17 20:12:12 -0500

    swap case

--- resources/views/partials/generic_content_block/partials/button.blade.php
+++ resources/views/partials/generic_content_block/partials/button.blade.php
@@ -1,7 +1,7 @@
 <div class="grid-x generic-item-wrapper">
     <div class="cell">
         <a href="{{ $button_link }}">
-            <button class="hollow button" target="{{ $open_in_new_window ? '_self' : '_blank'}}">{!! $button_text !!}</button>
+            <button class="hollow button" target="{{ $open_in_new_window ? '_blank' : '_self'}}">{!! $button_text !!}</button>
         </a>
     </div>
 </div>
