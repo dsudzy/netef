c1035ed8d8e09dab6e476b26dd651d3902d4c44d
Dan Sudenfield <dansudenfield@Dans-MBP.hsd1.ma.comcast.net>
2021-02-17 20:30:32 -0500

    put target on href

--- resources/views/partials/generic_content_block/partials/button.blade.php
+++ resources/views/partials/generic_content_block/partials/button.blade.php
@@ -1,7 +1,7 @@
 <div class="grid-x generic-item-wrapper">
     <div class="cell">
-        <a href="{{ $button_link }}">
-            <button class="hollow button" target="{{ $open_in_new_window ? '_blank' : '_self'}}">{!! $button_text !!}</button>
+        <a href="{{ $button_link }}" target="{{ $open_in_new_window ? '_blank' : '_self'}}">
+            <button class="hollow button">{!! $button_text !!}</button>
         </a>
     </div>
 </div>
