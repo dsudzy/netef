bd6f805cafb07ab742bf02fe83ae972778989f47
Dan Sudenfield <dansudenfield@Dans-MacBook-Pro.local>
2021-02-18 21:11:22 -0500

    clear views

--- resources/views/partials/generic_content_block/partials/button.blade.php
+++ resources/views/partials/generic_content_block/partials/button.blade.php
@@ -1,6 +1,6 @@
 <div class="grid-x generic-item-wrapper">
     <div class="cell">
-        <a href="{{ $button_link }}" target="{{ $open_in_new_window ? '_blank' : '_self'}}">
+        <a href="{{ $button_link }}" target="{{ $open_in_new_tab ? '_blank' : '_self'}}">
             <button class="hollow button">{!! $button_text !!}</button>
         </a>
     </div>
