33e75d991eac9c7044e0d5140040780df8e70225
Dan Sudenfield <dansudenfield@Dans-MBP.hsd1.ma.comcast.net>
2021-02-21 21:50:23 -0500

    responsive iframe

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -18,7 +18,10 @@
 
 <div class="header-img-container">
     @if(!empty($meta_data['vimeo_embed_link']))
-        <iframe src="{{ $meta_data['vimeo_embed_link'] }}" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+        <div style="padding:56.25% 0 0 0;position:relative;">
+            <iframe src="{{ $meta_data['vimeo_embed_link'] }}?title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <script src="https://player.vimeo.com/api/player.js"></script>
     @elseif(!empty($meta_data['header_image']))
         <div class="bg-img-top" style="background-image:url( {{ $image->getImageUrl($meta_data['header_image'] ?? 0) }} )"></div>
         <img class="header-img" src="{{ $image->getImageUrl($meta_data['header_image'] ?? 0) }}" alt="header image">
