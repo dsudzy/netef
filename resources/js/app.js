5679d140242cf0578189f714d7200f828d21284d
Dan Sudenfield <dansudenfield@Dans-MBP.fios-router.home>
2021-02-23 18:44:49 -0500

    animation on desktop

--- resources/js/app.js
+++ resources/js/app.js
@@ -41,21 +41,45 @@ jQuery(function() {
 $('.mv-item-wrapper').on('click', function(e) {
     e.preventDefault();
     e.stopPropagation();
-    $(this).css({ "overflow": "visible" });
-    if (window.innerWidth > 767) {
-        $('.middle').css({ "margin": 0 })
-    }
-    $($(this).find('.mv-close')).show()
+    $(this).css({
+        "overflow": "visible",
+    });
+    $($(this).find('.mv-text')).hide()
+    $($(this).find('.mv-item')).css({ width: '20rem' })
+    $($(this).find('.mv-item')).animate({
+        width: '54rem',
+    }, 1500, function() {
+        if (window.innerWidth > 767) {
+            $('.middle').css({ "margin": 0 })
+        }
+        $($(this).find('.mv-text')).fadeIn("slow")
+        $($(this).find('.mv-close')).show()
+
+    });
+
+
+
 });
 
 $(".mv-close").on('click', function(e) {
     e.preventDefault();
     e.stopPropagation();
-    $('.mv-item-wrapper').css({ "overflow": "hidden" });
-    if (window.innerWidth > 767) {
-        $('.middle').css({ "margin": '0 1rem' })
-    }
-    $(this).hide()
+    $($(this).siblings()[1]).fadeOut("slow", () => {
+        $(this).fadeOut("slow", () => {
+            $($(this).closest('.mv-item')).animate({
+                width: '20rem'
+            }, 1500, () => {
+                $('.mv-text').css({ 'width': '32rem;' })
+                $('.mv-text').show()
+                $('.mv-item-wrapper').css({ "overflow": "hidden" });
+                $('.mv-item').css({ "width": "54rem" });
+                if (window.innerWidth > 767) {
+                    $('.middle').css({ "margin": '0 1rem' })
+                }
+            })
+        });
+    })
+
 });
 
 var draggie = new Draggabilly('.draggable', {});
