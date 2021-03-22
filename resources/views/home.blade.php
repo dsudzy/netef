fd23e046eaa3b0043e29dff38493f65ef2c40e22
Dan Sudenfield <dansudenfield@Dans-MacBook-Pro.local>
2021-03-21 22:30:40 -0400

    cursor pointer, allow image to be click point

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -32,7 +32,7 @@
         <div class="play-button">
             <i class="far fa-play-circle"></i>
         </div>
-        <div id="header-image header-image-playable" class="small-12 small-order-2 medium-order-1 large-6 medium-6 cell header-image">
+        <div class="small-12 small-order-2 medium-order-1 large-6 medium-6 cell header-image header-image-playable">
             <img src="{{ $image->getImageUrl($meta_data['left_image']) }}">
             <div style="padding:56.25% 0 0 0;position:relative;" class="vimeo-teaser">
                 <iframe src="{{ $meta_data['vimeo_embed_link'] }}?title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
